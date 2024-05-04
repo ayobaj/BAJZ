4e796e378ba9566ca293a9ba31dbbd69b6ec1200
ayo baj <ayobaj007@gmail.com>
2024-05-04 13:26:55 +0100

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
 
 const App = () => {
 return (
     <div>
-        BAJZ
+
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/sign-in' element={<SignIn/>} />
+            <Route path='/sign-up' element={<SignUp/>} />
+            <Route path='/dashboard' element={<Dashboard/>} />
+            <Route path='/projects' element={<Projects/>} />
+        </Routes>
+        
     </div>
 )
 }
