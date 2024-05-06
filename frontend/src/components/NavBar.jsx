d26f8a0107bee8ce2951d29494d4516d70f536a6
ayo baj <ayobaj007@gmail.com>
2024-05-06 16:06:06 +0100

    navbar completion

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,68 @@
-import { Navbar, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 import image from "../assets/nav.png"
 import {AiOutlineSearch} from "react-icons/ai";
-import {FaMoon} from 'react-icons/fa';
+import { RiMenu4Line } from "react-icons/ri";
+import { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
 
 
 const NavBar = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleLinkClick = () => {
+    setShow(false);
+  }
+
   return (
-    <div>
-      <Navbar className="border-b-2 ">
+    <div className="">
+      <nav className="md:border-b-2 flex items-center justify-between md:mx-auto p-3">
 
-        <Link to='/' className="self-center text-sm 
-          sm:text-xl font-semibold dark:text-white px-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 ">
-          <img src={image} className="w-[35px] sm:w-[50px] rounded-lg "/>
-        </Link>
+        <div className="flex items-center">
 
+          {show === false ? <RiMenu4Line onClick={()=>setShow(true)} className="cursor-pointer text-3xl md:hidden text-indigo-400 "/> :
+          <IoCloseSharp onClick={()=>setShow(false)} className="cursor-pointer text-3xl md:hidden text-indigo-400"/>}
+
+          {show && <div className="md:hidden text-white font-semibold text-xl md:static md:min-h-fit absolute md:w-auto bg-indigo-400 md:bg-white min-h-[50vh] left-0 top-[9.7%] w-full flex items-center justify-center">
+          <div className="flex items-center md:gap-[50px] md:flex-row flex-col gap-10">
+            <Link to={'/'} onClick={handleLinkClick} >
+              Home
+            </Link>
+
+            <Link to={'/about'} onClick={handleLinkClick}>
+              About
+            </Link>
+
+            <Link to={'/projects'} onClick={handleLinkClick}>
+              Projects 
+            </Link>
+          </div>
+          </div> }
+
+          <Link to='/' className="self-center text-sm 
+            sm:text-xl font-semibold dark:text-white px-2 text-transparent bg-clip-text ">
+            <img src={image} className="w-[35px] sm:w-[50px] rounded-lg"/>
+          </Link>
+        </div>
+
+        <div className="md:static md:min-h-fit absolute md:w-auto bg-indigo-400 md:bg-white min-h-[60vh] left-0 top-[-100%] w-full flex items-center justify-center">
+          <div className="flex items-center md:gap-[50px] md:flex-row flex-col gap-10 font-semibold text-lg text-indigo-400">
+            <Link to={'/'}>
+              Home
+            </Link>
+
+            <Link to={'/about'}>
+              About
+            </Link>
+
+            <Link to={'/projects'}>
+              Projects 
+            </Link>
+          </div>
+        </div>
         
-        <div className="space-x-5 flex item-center">
+        <div className="space-x-3 flex item-center">
 
         
             <form className="flex items-center">
@@ -24,16 +70,12 @@ const NavBar = () => {
                 <AiOutlineSearch/>
               </button>
 
-              <TextInput type="text" 
-              rightIcon={AiOutlineSearch}
-              placeholder="Search..." 
-              className="hidden md:inline focus:outline-none"/>
+              <div className="hidden md:flex relative">
+                <input className="border-2 p-2 pl-7 focus:outline-none border-indigo-400" placeholder="Search..."/>
+                <AiOutlineSearch className="absolute text-xl text-indigo-400 top-3.5 left-1 "/>
+              </div>
             </form>
           
-
-          <button className="hidden sm:inline"> 
-            <FaMoon/>
-          </button>
 
           <Link to='/sign-in' className="">
             <button className="btn">
@@ -41,9 +83,10 @@ const NavBar = () => {
             </button>
           </Link>
 
+
         </div>
         
-      </Navbar>
+      </nav>
     </div>
   )
 }
