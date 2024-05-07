a29479498f6dd3334135662abd5365aea7df4710
ayo baj <ayobaj007@gmail.com>
2024-05-07 14:33:49 +0100

    MonogDB connected

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 
+
+dotenv.config();
 const app = express();
 const port = 5000;
 
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=> console.log('DataBase is connected!'))
+.catch((err) => {
+    console.log(err)
+})
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`)
