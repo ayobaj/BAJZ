0c03a7d62642d98546f1c42e74b8d56fdffd3b9c
ayo baj <ayobaj007@gmail.com>
2024-05-12 20:30:22 +0100

    redux persist one

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,5 +1,18 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userReducer from '../Redux/user/userSlice'
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import userReducer from '../Redux/user/userSlice';
+import {persistReducer} from 'redux-persist';
+
+
+const rootReducer = combineReducers({
+    reducer: {
+        user: userReducer,
+    }
+})
+
+const persistedReducer = persistReducer({
+
+})
+
 
 
 export const store = configureStore({
