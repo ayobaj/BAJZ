1b80935fc50d4cbcf647efc0460906598bd13303
ayo baj <ayobaj007@gmail.com>
2024-05-29 22:28:54 +0100

    image fileupload functionality completed

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import {useSelector} from 'react-redux';
-import {getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase' 
+import { Alert } from 'flowbite-react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 
 
@@ -37,6 +40,7 @@ const DashProfile = () => {
     }, [imageFile]);
 
     const uploadImage = async () => {
+        setImageFileUploadError(null);
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
         const storageRef = ref(storage, fileName);
@@ -50,14 +54,37 @@ const DashProfile = () => {
             }, 
 
             (error) => {
-                setImageFileUploadError('Could not upload image(File size must be less than 2MB)')
+                let errorMessage;
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        errorMessage = 'Permission denied';
+                        break;
+                    case 'storage/canceled':
+                        errorMessage = 'Upload Cancelled';
+                        break;
+                    case 'storage/unknown':
+                        errorMessage = 'Unknown error occurred. Please try again.';
+                        break;
+                    default:
+                        errorMessage = 'File size must be less than 2MB';
+                        break;
+                }
+                setImageFileUploadError(errorMessage);
+                setImageFileUploadProgress(null);
+                setImageFile(null);
+                setImageFileUrl(null);
             },
-            
-            
+
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setImageFileUrl(downloadURL);
+                    setImageFileUploadProgress(null);
+                })
+            }
         )
     }
 
-    console.log(imageFile, imageFileUrl)
+
 
     return (
         <div className='max-w-sm mx-auto p-3 w-full'>
@@ -70,9 +97,29 @@ const DashProfile = () => {
 
                 <input hidden type='file' accept='image/*' onChange={handleImageChange} ref={filePicker}/>
                 
-                <div className='w-32 h-30 self-center cursor-pointer shadow-md rounded-full overflow-hidden' onClick={()=> filePicker.current.click()}>
-                    <img className='rounded-full w-full h-full border-8 border-indigo-300 object-cover' src={imageFileUrl || currentUser.profilePicture} alt='user image'/>
+                <div className='relative w-32 h-30 self-center cursor-pointer shadow-md rounded-full overflow-hidden' onClick={()=> filePicker.current.click()}>
+
+                    {imageFileUploadProgress && (<CircularProgressbar value={imageFileUploadProgress || 0}
+                        text={`${imageFileUploadProgress}%`} 
+                        strokeWidth={5} styles={{
+                        root:{
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0
+                        },
+                        path:{
+                            stroke: `rgba(165, 180, 252, ${imageFileUploadProgress / 100})`,
+                        }
+                    }}
+                    />)}
+
+                    <img className= {`rounded-full w-full h-full border-8 border-slate-200 object-cover ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'} `} src={imageFileUrl || currentUser.profilePicture} alt='user image'/>
+
                 </div>
+
+                {imageFileUploadError && (<Alert color='failure'>{imageFileUploadError}</Alert>)}
 
                 <input type='text' id='username'
                 placeholder='username'
