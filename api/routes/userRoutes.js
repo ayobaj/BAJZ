7cd09042931e90c7c98a43086d7b63a2ea790256
ayo baj <ayobaj007@gmail.com>
2024-06-13 22:24:53 +0100

    created the getusers api route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signOut } from '../controllers/userController.js';
+import { test, updateUser, deleteUser, signOut, getUsers } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,13 @@ router.get('/test',test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signOut);
+
+// route to get the users information, visible only to the admin
+router.get('/getusers', verifyToken, getUsers);
+
+
+
+
+
 
 export default router;
