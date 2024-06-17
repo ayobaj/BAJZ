0c88c8b4f332225374632a4225173f1c575d6c13
ayo baj <ayobaj007@gmail.com>
2024-06-17 18:28:20 +0100

    completed the route and controller for the delete button on the comment section

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js'
-import { createComment, getPostComment, likeComment, editComment } from '../controllers/commentController.js';
+import { createComment, getPostComment, likeComment, editComment, deleteComment } from '../controllers/commentController.js';
 
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.get('/getPostComment/:postId', getPostComment);
 
 router.put('/likeComment/:commentId', verifyToken, likeComment)
 
-router.put('/editComment/:commentId', verifyToken, editComment)
+router.put('/editComment/:commentId', verifyToken, editComment);
+
+router.put('/deleteComment/:commentId', verifyToken, deleteComment)
 
 export default router;
