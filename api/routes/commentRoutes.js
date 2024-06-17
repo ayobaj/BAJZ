96d2baabeb7f2be302ecc4affddb946ffaae9450
ayo baj <ayobaj007@gmail.com>
2024-06-17 16:49:47 +0100

    created the routes and controller for the edit comment button

--- api/routes/commentRoutes.js
+++ api/routes/commentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js'
-import { createComment, getPostComment, likeComment } from '../controllers/commentController.js';
+import { createComment, getPostComment, likeComment, editComment } from '../controllers/commentController.js';
 
 
 const router = express.Router();
@@ -10,5 +10,7 @@ router.post('/create', verifyToken,  createComment);
 router.get('/getPostComment/:postId', getPostComment);
 
 router.put('/likeComment/:commentId', verifyToken, likeComment)
+
+router.put('/editComment/:commentId', verifyToken, editComment)
 
 export default router;
