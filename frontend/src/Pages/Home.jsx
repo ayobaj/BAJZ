9928eb7d0eda5fcbda277bd9ab2ed7273d10914e
ayo baj <ayobaj007@gmail.com>
2024-06-25 14:35:31 +0100

    home page landing page and little changes to the navbar

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,76 @@
-
+import {Link} from 'react-router-dom';
+import { FaArrowDown, FaArrowRightLong } from "react-icons/fa6";
+import { useEffect, useState } from 'react';
+import PostCard from '../components/PostCard';
 
 const Home = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try{
+
+                const res = await fetch('/api/post/getPosts');
+                const data = await res.json();
+                setPosts(data.posts);
+
+            }catch(error){
+                console.log(error.message)
+            }
+        }
+
+        fetchPosts();
+    }, []);
+
+
 return (
-    <div>
-        Home
+    <div className=''>
+
+        <div className="flex flex-col gap-5 p-12  sm:p-28 max-w-6xl h-[500px] justify-between lg:min-h-screen">
+
+            <div className='space-y-2'>
+                <h1 className="text-5xl font-bold lg:text-7xl text-slate-600">BAJZ BLOG</h1>
+
+                <p className="text-gray-500 text-sm sm:text-sm">
+                Read our latest articles and posts on technology, 
+                fashion, and entertainment.
+                </p>
+            </div>
+
+            <div className='flex justify-end lg:ml-[1100px]'>
+                    <button className='btn p-4 flex'><FaArrowDown className='text-xl'/></button>
+            </div>
+
+        </div>
+
+
+        <div className='flex justify-end items-center pr-12'>
+            <Link to='/search'>
+                <button className='update flex items-center justify-between gap-4 w-[200px] h-[50px] sm:w-[200px]'>ALL
+                    <span><FaArrowRightLong className='text-2xl'/></span>
+                </button>
+            </Link>
+        </div>
+
+        <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+
+            {posts && posts.length > 0 && (
+
+                <div>
+
+                    <div className='flex flex-wrap gap-4 justify-center items-center'>
+                        {posts.map((post) => (
+                            <PostCard key={post._id} post={post}/>
+                        ))}
+                    </div>
+
+                </div>
+            )}
+
+        </div>
+
+
     </div>
 )
 }
--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const PostCard = ({post}) => {
 return (
-    <div className='mb-4 mt-3 group relative w-[300px] sm:h-[320px] overflow-hidden rounded-lg sm:w-[290px] border border-indigo-400 hover:border-2 transition-all'>
+    <div className='mb-4 mt-3 group relative w-[300px] sm:h-[320px] overflow-hidden  sm:w-[290px] border border-indigo-400 hover:border-2 transition-all'>
         <Link to={`/post/${post.slug}`} className=''>
             <img src={post.image} 
             alt='post image'
@@ -17,8 +17,8 @@ return (
             <span className='ml-3 italic text-sm'>{post.category}</span>
 
             <Link to={`/post/${post.slug}`} className='hidden sm:block sm:z-10 sm:group-hover:bottom-0 absolute sm:bottom-[-200px] 
-            sm:left-0 sm:right-0 sm:border sm:border-indigo-500 sm:text-indigo-400 sm:hover:bg-indigo-400 sm:hover:text-white
-            sm:transition-all sm:duration-300 sm:text-center sm:py-2 sm:rounded-md sm:m-2'>
+            sm:left-0 sm:right-0 sm:border sm:border-indigo-500  sm:hover:bg-indigo-400 sm:hover:text-white
+            sm:transition-all sm:duration-300 sm:text-center sm:py-2 sm:m-2'>
                 Read Post
             </Link>
         </div>
