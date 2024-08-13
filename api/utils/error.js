e0c1d53df83bfd0f5ba3bcf2a0a8d8544080e01d
ayo baj <ayobaj007@gmail.com>
2024-08-13 14:08:51 +0100

    docs: comment on the custom error.

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,3 +1,4 @@
+// Custom error that can be passed by the middleware to prevent the repetition of typing error
 export const errorHandler = (statusCode, message) => {
     const error =new Error();
     error.statusCode = statusCode;
