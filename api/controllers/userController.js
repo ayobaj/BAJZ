b5f68c8cb11f73a91a870668c08ada8ad8b60544
ayo baj <ayobaj007@gmail.com>
2024-10-07 16:04:15 +0100

    fix: got rid of a test function in the user controller that wasn't in use

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -4,9 +4,7 @@ import User from '../models/userModel.js';
 
 
 
-export const test =  (req, res) => {
-    res.json({message: 'server is running'})
-}
+
 
 
 export const updateUser = async (req, res, next) => {
