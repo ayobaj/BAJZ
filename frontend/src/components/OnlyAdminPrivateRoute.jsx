e67fadddb831389eb7d82959428214b7ba00ddb6
ayo baj <ayobaj007@gmail.com>
2024-10-16 18:47:49 +0100

    doc: comment explaining why i used outlet instead of passing whatever is insided the OnlyAdminPrivateRoutes as a children

--- frontend/src/components/OnlyAdminPrivateRoute.jsx
+++ frontend/src/components/OnlyAdminPrivateRoute.jsx
@@ -8,6 +8,9 @@ const OnlyAdminPrivateRoute = () => {
 
     return (
         <div>
+
+            {/*I made use of Outlet because it is an alternative instead of passing children into it*/}
+
             {currentUser && currentUser.isAdmin ? <Outlet/> : <Navigate to='/sign-in'/> }
         </div>
     )
