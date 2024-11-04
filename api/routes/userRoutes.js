7a9439bb06e9b83c046faa7e20c35e3eafc1914e
ayo baj <ayobaj007@gmail.com>
2024-11-04 21:28:57 +0100

    fix: got rid of a test route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,7 +4,6 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get('/test',test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signOut);
